6a05b1912f4296130bbf57ee3dc07104e7f4ecb3
moemaair <rageali12@gmail.com>
2024-01-06 17:04:46 +0300

    add env file with configuration

--- src/main/java/app/netlify/mohamedi/movies/vomie/VomieApplication.java
+++ src/main/java/app/netlify/mohamedi/movies/vomie/VomieApplication.java
@@ -2,8 +2,11 @@ package app.netlify.mohamedi.movies.vomie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VomieApplication {
 
 	public static void main(String[] args) {
