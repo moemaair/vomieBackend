ff5ad2a9e2041e1bc11d2e6bff7e554b9933e0e6
moemaair <rageali12@gmail.com>
2024-01-06 15:47:46 +0300

    initial commit

---
+++ src/main/java/app/netlify/mohamedi/movies/vomie/VomieApplication.java
@@ -0,0 +1,13 @@
+package app.netlify.mohamedi.movies.vomie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VomieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VomieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/netlify/mohamedi/movies/vomie/VomieApplicationTests.java
@@ -0,0 +1,13 @@
+package app.netlify.mohamedi.movies.vomie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VomieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
