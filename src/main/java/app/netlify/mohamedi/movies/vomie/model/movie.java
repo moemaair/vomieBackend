5b596604f75cc8e0c2d1dea700dd88d327094b6a
moemaair <rageali12@gmail.com>
2024-01-07 09:54:22 +0300

    add many to many relationship with reviews using @DocumentReference

--- src/main/java/app/netlify/mohamedi/movies/vomie/model/movie.java
+++ src/main/java/app/netlify/mohamedi/movies/vomie/model/movie.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
@@ -23,5 +24,6 @@ public class movie {
     private List<String> genres;
     private String poster;
     private List<String> backdrops;
+    @DocumentReference
     private List<Review> reviewIds;
 }
