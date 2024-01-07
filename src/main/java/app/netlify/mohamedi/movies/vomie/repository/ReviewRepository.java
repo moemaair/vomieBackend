d6cfdea696ef70d088e861c2b09bf626f3687126
moemaair <rageali12@gmail.com>
2024-01-07 20:46:37 +0300

    logic of posting a new review to a specific movie using imdbid

--- src/main/java/app/netlify/mohamedi/movies/vomie/repository/ReviewRepository.java
+++ src/main/java/app/netlify/mohamedi/movies/vomie/repository/ReviewRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface ReviewRepository extends MongoRepository<Review, ObjectId>{
+public interface ReviewRepository extends MongoRepository<Review, ObjectId>{
 
 }
--- src/main/java/app/netlify/mohamedi/movies/vomie/service/ReviewService.java
+++ src/main/java/app/netlify/mohamedi/movies/vomie/service/ReviewService.java
@@ -1,4 +1,32 @@
 package app.netlify.mohamedi.movies.vomie.service;
 
+import app.netlify.mohamedi.movies.vomie.model.Movie;
+import app.netlify.mohamedi.movies.vomie.model.Review;
+import app.netlify.mohamedi.movies.vomie.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviews").value(review))
+                .first();
+
+        return review;
+    }
 }
